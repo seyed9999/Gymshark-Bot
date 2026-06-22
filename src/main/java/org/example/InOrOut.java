045f1c6f4eb3e8bd35e21a911eb8e7513c445282
benjamin <benjamin.seyedhashem@gmail.com>
2026-06-22 21:57:49 +0200

    Fix stock detection by using lastIndexOf instead of contains

--- src/main/java/org/example/InOrOut.java
+++ src/main/java/org/example/InOrOut.java
@@ -8,38 +8,42 @@ import java.io.IOException;
 
 public class InOrOut {
 
-    public String[] checkClothes(String size,String url) {
+    public String[] checkClothes(String size, String url) {
 
-            Document doc = null;
-            try {
-                doc = Jsoup.connect(url).get();
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-            if (doc == null) {
-                return null;
-            }
-            String data = doc.select("script#__NEXT_DATA__").first().data();
-            if (data.contains("\"size\":\""+ size +"\",\"inStock\":true")) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        if (doc == null) {
+            return null;
+        }
+        String data = doc.select("script#__NEXT_DATA__").first().data();
+        int sizeIndex = data.indexOf("\"size\":\"" + size + "\"");
+        if (sizeIndex != -1) {
+            String before = data.substring(sizeIndex - 150, sizeIndex);
+            if (before.lastIndexOf("\"inStock\":true") > before.lastIndexOf("\"inStock\":false")) {
                 String name = doc.title();
                 Element priceText = doc.select(".product-information_price__LJWYG div").first();
-                if(priceText == null) {throw  new NullPointerException("price text is null");}
-                String priceCurrent = priceText.text().replace("€", "").replace(",",".").trim();
+                if (priceText == null) {
+                    throw new NullPointerException("price text is null");
+                }
+                String priceCurrent = priceText.text().replace("€", "").replace(",", ".").trim();
                 Element priceTextOr = doc.select(".product-information_compare-at-price__gN_Bb").first();
                 String priceOriginal;
-                if(priceTextOr != null) {
-                     priceOriginal = priceTextOr.text().replace("€", "").replace(",",".").trim();
+                if (priceTextOr != null) {
+                    priceOriginal = priceTextOr.text().replace("€", "").replace(",", ".").trim();
                 } else {
-                     priceOriginal = priceCurrent;
+                    priceOriginal = priceCurrent;
                 }
 
                 double result = Double.parseDouble(priceCurrent) - Double.parseDouble(priceOriginal);
 
-                String[] info = {size, name, priceCurrent,result + ""};
+                String[] info = {size, name, priceCurrent, result + ""};
                 return info;
             }
-
+        }
         return null;
     }
-
 }
