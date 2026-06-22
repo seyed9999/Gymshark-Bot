dd304eaca8383f77ab0a94a68120b880a6171d95
benjamin <benjamin.seyedhashem@gmail.com>
2026-06-22 21:28:39 +0200

    Improved my past Errors wher it only could check the first product of my URL that I wrote in my programm but now it should function properly

--- src/main/java/org/example/CallGymshark.java
+++ src/main/java/org/example/CallGymshark.java
@@ -7,31 +7,40 @@ import java.util.concurrent.TimeUnit;
 public class CallGymshark {
     private final int time = 2;
     private String[] sizes = {"s", "m"};
-    private String[][] inStocks = new String[sizes.length][];
-    private boolean[] once = new boolean[sizes.length];
+    private final String[] GymsharkUrls =
+            {"https://de.gymshark.com/products/gymshark-shadow-seamless-t-shirt-black-aw24",
+                    "https://de.gymshark.com/products/gymshark-geo-seamless-t-shirt-black-charcoal-grey-ss23",
+                    "https://de.gymshark.com/products/gymshark-element-baselayer-t-shirt-black-ss25"};
+    private String[][][] inStocks = new String[GymsharkUrls.length][sizes.length][];
+    private boolean[][] once = new boolean[GymsharkUrls.length][sizes.length];
     private String[] prices = new String[sizes.length];
+
 
     public void startBot() {
         InOrOut inOrOut = new InOrOut();
         Notifier notifier = new Notifier();
         ScheduledExecutorService worker = Executors.newScheduledThreadPool(1);
         worker.scheduleAtFixedRate(() -> {
-            for (int i = 0; i < sizes.length; i++) {
-                inStocks[i] = inOrOut.checkClothes(sizes[i]);
-                if (inStocks[i] != null && !once[i]) {
-                    if (Integer.parseInt(inStocks[i][3]) < 0) {
-                        notifier.sendMsg("The product: " + inStocks[i][1] + "is back in stock in the size of " + inStocks[i][0] +
-                                " and is also in sale in the price of " + inStocks[i][2]);
-                    } else {
-                        notifier.sendMsg("The product: " + inStocks[i][1] + "is back in stock in the size of " + inStocks[i][0]);
+            for (int i = 0; i < GymsharkUrls.length; i++) {
+                for (int j = 0; j < sizes.length; j++) {
+                    inStocks[i][j] = inOrOut.checkClothes(sizes[j], GymsharkUrls[i]);
+                    if (inStocks[i][j] != null && !once[i][j]) {
+                        if (Double.parseDouble(inStocks[i][j][3]) < 0) {
+                            notifier.sendMsg("The product: " + inStocks[i][j][1] + "is back in stock in the size of " + inStocks[i][j][0] +
+                                    " and is also in sale in the price of " + inStocks[i][j][2]);
+                        } else {
+                            notifier.sendMsg("The product: " + inStocks[i][j][1] + "is back in stock in the size of " + inStocks[i][j][0] + "in the price of "
+                                    + inStocks[i][j][2]);
 
+
+                        }
+                        once[i][j] = true;
                     }
-                    once[i] = true;
-                }
 
 
-                if (inStocks[i] == null) {
-                    once[i] = false;
+                    if (inStocks[i][j] == null) {
+                        once[i][j] = false;
+                    }
                 }
             }
         }, 0, time, TimeUnit.MINUTES);
--- src/main/java/org/example/InOrOut.java
+++ src/main/java/org/example/InOrOut.java
@@ -2,22 +2,17 @@ package org.example;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 
 public class InOrOut {
-    private final String[] GymsharkUrls =
-            {"https://de.gymshark.com/products/gymshark-shadow-seamless-t-shirt-black-aw24",
-                    "https://de.gymshark.com/products/gymshark-geo-seamless-t-shirt-black-charcoal-grey-ss23",
-                    "https://de.gymshark.com/products/gymshark-element-baselayer-t-shirt-black-ss25"};
-    private String[] sizes = {"s", "m"};
 
-    public String[] checkClothes(String size) {
-        for (int i = 0; i < GymsharkUrls.length; i++) {
+    public String[] checkClothes(String size,String url) {
 
             Document doc = null;
             try {
-                doc = Jsoup.connect(GymsharkUrls[i]).get();
+                doc = Jsoup.connect(url).get();
             } catch (IOException e) {
                 System.out.println(e.getMessage());
             }
@@ -25,19 +20,25 @@ public class InOrOut {
                 return null;
             }
             String data = doc.select("script#__NEXT_DATA__").first().data();
-            if (data.contains("\"size\":\"" + size + "\"")
-                    && data.contains("\"inStock\":true")) {
+            if (data.contains("\"size\":\""+ size +"\",\"inStock\":true")) {
                 String name = doc.title();
-                String priceText = doc.select(".product-information_price__LJWYG div").first().text();
-                String priceCurrent = priceText.replace("€", "").trim();
-                String priceTextO = doc.select(".product-information_compare-at-price__gN_Bb").first().text();
-                String priceOriginal = priceText.replace("€", "").trim();
-                int result = Integer.parseInt(priceCurrent) - Integer.parseInt(priceOriginal);
+                Element priceText = doc.select(".product-information_price__LJWYG div").first();
+                if(priceText == null) {throw  new NullPointerException("price text is null");}
+                String priceCurrent = priceText.text().replace("€", "").replace(",",".").trim();
+                Element priceTextOr = doc.select(".product-information_compare-at-price__gN_Bb").first();
+                String priceOriginal;
+                if(priceTextOr != null) {
+                     priceOriginal = priceTextOr.text().replace("€", "").replace(",",".").trim();
+                } else {
+                     priceOriginal = priceCurrent;
+                }
+
+                double result = Double.parseDouble(priceCurrent) - Double.parseDouble(priceOriginal);
 
                 String[] info = {size, name, priceCurrent,result + ""};
                 return info;
             }
-        }
+
         return null;
     }
 
