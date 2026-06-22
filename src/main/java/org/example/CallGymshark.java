aed8ac5709e0b76a1e0541577fa6c3987645b4d6
benjamin <benjamin.seyedhashem@gmail.com>
2026-06-22 10:19:07 +0200

    I added a few extra changes in my project: tracking more than just one size and show wich size from one product is available, track more than just one product and show which product is available by name, track if the products are on sale or not and now it jus t sends you one message only once per product

--- src/main/java/org/example/CallGymshark.java
+++ src/main/java/org/example/CallGymshark.java
@@ -6,14 +6,36 @@ import java.util.concurrent.TimeUnit;
 
 public class CallGymshark {
     private final int time = 2;
+    private String[] sizes = {"s", "m"};
+    private String[][] inStocks = new String[sizes.length][];
+    private boolean[] once = new boolean[sizes.length];
+    private String[] prices = new String[sizes.length];
+
     public void startBot() {
         InOrOut inOrOut = new InOrOut();
         Notifier notifier = new Notifier();
         ScheduledExecutorService worker = Executors.newScheduledThreadPool(1);
         worker.scheduleAtFixedRate(() -> {
-            if(inOrOut.checkClothes()) {
-                notifier.sendMsg("Product is back in stock");
+            for (int i = 0; i < sizes.length; i++) {
+                inStocks[i] = inOrOut.checkClothes(sizes[i]);
+                if (inStocks[i] != null && !once[i]) {
+                    if (Integer.parseInt(inStocks[i][3]) < 0) {
+                        notifier.sendMsg("The product: " + inStocks[i][1] + "is back in stock in the size of " + inStocks[i][0] +
+                                " and is also in sale in the price of " + inStocks[i][2]);
+                    } else {
+                        notifier.sendMsg("The product: " + inStocks[i][1] + "is back in stock in the size of " + inStocks[i][0]);
+
+                    }
+                    once[i] = true;
+                }
+
+
+                if (inStocks[i] == null) {
+                    once[i] = false;
+                }
             }
-        },0,time, TimeUnit.MINUTES);
+        }, 0, time, TimeUnit.MINUTES);
     }
 }
+
+
--- src/main/java/org/example/InOrOut.java
+++ src/main/java/org/example/InOrOut.java
@@ -6,24 +6,39 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 
 public class InOrOut {
-    private final String GymsharkUrl = "https://de.gymshark.com/products/gymshark-shadow-seamless-t-shirt-black-aw24";
+    private final String[] GymsharkUrls =
+            {"https://de.gymshark.com/products/gymshark-shadow-seamless-t-shirt-black-aw24",
+                    "https://de.gymshark.com/products/gymshark-geo-seamless-t-shirt-black-charcoal-grey-ss23",
+                    "https://de.gymshark.com/products/gymshark-element-baselayer-t-shirt-black-ss25"};
+    private String[] sizes = {"s", "m"};
 
-    public boolean checkClothes() {
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(GymsharkUrl).get();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        if (doc == null) {
-            return false;
-        }
-        String data = doc.select("script#__NEXT_DATA__").first().data();
-        if (data.contains("\"inStock\":true,\"inventoryQuantity\":2,\"price\":32,\"size\":\"s\"")) {
-            return true;
-        } else {
-            return false;
-        }
+    public String[] checkClothes(String size) {
+        for (int i = 0; i < GymsharkUrls.length; i++) {
 
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(GymsharkUrls[i]).get();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            if (doc == null) {
+                return null;
+            }
+            String data = doc.select("script#__NEXT_DATA__").first().data();
+            if (data.contains("\"size\":\"" + size + "\"")
+                    && data.contains("\"inStock\":true")) {
+                String name = doc.title();
+                String priceText = doc.select(".product-information_price__LJWYG div").first().text();
+                String priceCurrent = priceText.replace("€", "").trim();
+                String priceTextO = doc.select(".product-information_compare-at-price__gN_Bb").first().text();
+                String priceOriginal = priceText.replace("€", "").trim();
+                int result = Integer.parseInt(priceCurrent) - Integer.parseInt(priceOriginal);
+
+                String[] info = {size, name, priceCurrent,result + ""};
+                return info;
+            }
+        }
+        return null;
     }
+
 }
